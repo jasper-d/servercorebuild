2351b43a144ee200adbe21e58e9e71f8f4618a9f
jasperd <jasper-d@users.noreply.github.com>
2019-06-22 17:25:31 +0200

    Add aspnetcore

--- build-local.ps1
+++ build-local.ps1
@@ -1,6 +1,8 @@
 $GitHash = "localbld12345678123456781234567812345678"
-Get-Content .\set-vars.ps1 -Tail 12 | Invoke-Expression
+Get-Content .\set-vars.ps1 -Tail 18 | Invoke-Expression
 $buildSdk = "cmd /S /C `"set ARG_DOTNET_SDK_VERSION=$SdkVersion && docker build -t servercorebuild:localbld --build-arg ARG_DOTNET_SDK_VERSION=$SdkVersion .`""
 $buildRuntime = "cmd /S /C `"set ARG_DOTNET_RUNTIME_VERSION=$RuntimeVersion && docker build -t servercoreruntime:localbld --build-arg ARG_DOTNET_RUNTIME_VERSION=$RuntimeVersion -f Dockerfile.runtime .`""
+$buildAspNetCore = "cmd /S /C `"set ARG_ASPNETCORE_RUNTIME_VERSION=$RuntimeVersion && docker build -t servercoreaspnetcore:localbld --build-arg ARG_ASPNETCORE_RUNTIME_VERSION=$RuntimeVersion -f Dockerfile.aspnetcore .`""
 iex $buildSdk
 iex $buildRuntime
+iex $buildAspNetCore
