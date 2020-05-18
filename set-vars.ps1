8d0f2f6c9751b2ffc53f90dcd6061d043887f3bc
Jasper <jasper-d@users.noreply.github.com>
2020-05-18 21:10:02 +0200

    Update set-vars.ps1
    Update SDK and frameworks 3.1.202/3.1.4

--- set-vars.ps1
+++ set-vars.ps1
@@ -4,13 +4,13 @@ Param(
 	[string]$GitHash
 )
 
-$SdkVersion = "3.1.102"
+$SdkVersion = "3.1.202"
 $hashTag = "sdk-git-$($GitHash.Substring(0, 8))"
 $sdkTag = "sdk-$SdkVersion"
-$RuntimeVersion = "3.1.2"
+$RuntimeVersion = "3.1.4"
 $runtimeHashTag = "runtime-git-$($GitHash.Substring(0, 8))"
 $runtimeTag = "runtime-$RuntimeVersion"
-$aspNetCoreVersion = "3.1.2"
+$aspNetCoreVersion = "3.1.4"
 $aspNetCoreHashTag = "aspnetcore-git-$($GitHash.Substring(0, 8))"
 $aspNetCoreTag = "aspnetcore-$RuntimeVersion"
 Write-Host "##vso[task.setvariable variable=SdkVersion]$SdkVersion"
